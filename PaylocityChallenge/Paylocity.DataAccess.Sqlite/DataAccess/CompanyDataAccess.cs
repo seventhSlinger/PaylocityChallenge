4a573c743c70981b7d84543ff8483c895fea137d
Chris Cimino <chris.cimino95@gmail.com>
2022-02-11 17:26:52 -0500

    - Added companyDataAccess

---
+++ PaylocityChallenge/Paylocity.DataAccess.Sqlite/DataAccess/CompanyDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using Common.Configuration;
+using Microsoft.Extensions.Options;
+using Paylocity.Models.Models;
+
+namespace Paylocity.DataAccess.Sqlite.DataAccess
+{
+    internal class CompanyDataAccess : BaseDataAccess<Company>
+    {
+        public CompanyDataAccess(IOptions<DatabaseConfig> config) : base("tblCompany", config)
+        { } 
+    }
+}
