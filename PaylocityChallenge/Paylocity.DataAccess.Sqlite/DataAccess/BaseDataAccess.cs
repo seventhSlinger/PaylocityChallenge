16a34b092b40d2c6472ce998cbf079cc493ddcfa
Chris Cimino <chris.cimino95@gmail.com>
2022-02-11 16:37:39 -0500

    - Added baseDataAccess class with basic CRUD
    - Added interface
    - Added package references

---
+++ PaylocityChallenge/Paylocity.DataAccess.Sqlite/DataAccess/BaseDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Common.Configuration;
+using Dapper;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Options;
+using Paylocity.Common.Interfaces;
+using Paylocity.DataAccess.Sqlite.Interfaces;
+using System.Collections.Generic;
+using Z.Dapper.Plus;
+
+namespace Paylocity.DataAccess.Sqlite.DataAccess
+{
+    internal abstract class BaseDataAccess<TModel> : IDataAccess<TModel>
+         where TModel : class, IUniqueModel
+    {
+        private readonly string _tableName;
+        private readonly string _connectionString;
+
+        public BaseDataAccess(string tableName,
+            IOptions<DatabaseConfig> config)
+        {
+            _tableName = tableName;
+            _connectionString = config.Value.ConnectionString;
+            DapperPlusManager.Entity<TModel>()
+                .Identity(entity => entity.Id)
+                .Table(tableName);
+        }
+
+        public virtual int Create(TModel model)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+
+            var models = new List<TModel> { model };
+            connection.BulkInsert(models);
+
+            return model.Id;
+        }
+
+        public virtual IEnumerable<TModel> List()
+        {
+            using var connection = new SqliteConnection(_connectionString);
+
+            return connection.Query<TModel>($"SELECT * FROM {_tableName}");
+
+        }
+    }
+}
---
+++ PaylocityChallenge/Paylocity.DataAccess.Sqlite/Interfaces/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using Paylocity.Common.Interfaces;
+using System.Collections.Generic;
+
+namespace Paylocity.DataAccess.Sqlite.Interfaces
+{
+    public interface IDataAccess<TModel> where TModel : class, IUniqueModel
+    {
+        int Create(TModel model);
+        IEnumerable<TModel> List();
+    }
+}
